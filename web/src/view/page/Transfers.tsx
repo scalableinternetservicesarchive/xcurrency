77531b6a3710619f0bb04eac8f17511016dcdd9b
dhruvsanchety <43064513+dhruvsanchety@users.noreply.github.com>
2020-11-11 20:48:32 -0800

    Update Transfers.tsx

--- web/src/view/page/Transfers.tsx
+++ web/src/view/page/Transfers.tsx
@@ -1,21 +1,41 @@
-/*
+import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
+import { ExchangeRequest } from '../../../../server/src/entities/ExchangeRequest'
+import { AppRouteParams } from '../nav/route'
+import { Page } from './Page'
 
-export function Transfers() {
-	const {requests} = React.useState([] as Request[])
+interface TransfersProps extends RouteComponentProps, AppRouteParams {}
 
-	fetch('requests')
-		.then(response => response.json())
-		.then(json => console.log(json))
-		.catch(err=>{
-			console.error(err)
-		})
-	return (
-	<div>
-		(requests.map(r=>(
-			<div key={r.x}</div>
-		))}
-	</div>
-	}
+export function Transfers(props: TransfersProps) {
+  return (
+    <Page>
+      <MyTransfers />
+    </Page>
+  )
 }
-*/
+
+export function MyTransfers() {
+  const [requests, setRequests] = React.useState([] as ExchangeRequest[])
+
+  fetch('/requests')
+    .then(response => response.json())
+    .then(json => setRequests(json))
+    .catch(err => {
+      console.error(err)
+    })
+
+  return (
+    <div>
+      <br></br>
+      <br></br>
+      {requests.reverse().map(r => (
+        <div key={r.requestId}>
+          Amount Paid: {r.amountPay} {r.fromCurrency}, Amount Wanted: {r.amountWant} {r.toCurrency}, Bid Rate:
+          {r.bidRate}
+          <br></br>
+          <br></br>
+        </div>
+      ))}
+    </div>
+  )
+}
