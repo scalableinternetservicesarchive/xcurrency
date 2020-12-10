413092f9aa14e7f9d7707a8d0920c2598805165c
Naveena Attota <naveena.attota@gmail.com>
2020-12-09 16:11:39 -0800

    placed the skeleton of subscription in the transfers page

--- web/src/view/page/Transfers.tsx
+++ web/src/view/page/Transfers.tsx
@@ -1,9 +1,14 @@
 import { useQuery, useSubscription } from '@apollo/client'
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
-import { FetchExchangeRequests, FetchExchangeRequestsVariables } from '../../graphql/query.gen'
+import {
+  FetchExchangeRequests,
+  FetchExchangeRequestsVariables,
+  RequestSubscription,
+  RequestSubscriptionVariables
+} from '../../graphql/query.gen'
 import { UserContext } from '../auth/user'
-import { fetchExchangeRequests } from '../exchangeRequestQL/fetchExchangeRequest'
+import { fetchExchangeRequests, subscribeRequests } from '../exchangeRequestQL/fetchExchangeRequest'
 import { AppRouteParams } from '../nav/route'
 import { Page } from './Page'
 interface TransfersProps extends RouteComponentProps, AppRouteParams {}
@@ -30,19 +35,53 @@ export function MyTransfers() {
   const id = user.displayId()
   const { loading, data } = useQuery<FetchExchangeRequests, FetchExchangeRequestsVariables>(fetchExchangeRequests, {
     variables: { id },
-    pollInterval: 1000,
+    // pollInterval: 1000,
   })
+
+  const [requestsUpdates, setRequestsUpdates] = React.useState(data?.exchangeRequests? as any)
+
+  useEffect(() => {
+    setRequestsUpdates(data?.exchangeRequests)
+  }, [data])
+
+  const sub = useSubscription<RequestSubscription, RequestSubscriptionVariables>(subscribeRequests, {
+    variables: { userId: user!.id },
+  })
+
+  // update according to subscription
+  useEffect(() => {
+    console.log(sub.data);
+    if (sub.data?.accountUpdates) {
+      if (userAccounts) {
+        const clonedUserAccounts: any[] = []
+        userAccounts.forEach((account: any) =>
+          clonedUserAccounts.push({ name: account.name, balance: account.balance })
+        )
+        let isUpdate = false
+        for (let i = 0; i < clonedUserAccounts.length; i++) {
+          if (clonedUserAccounts[i]?.name === sub.data?.accountUpdates.name) {
+            isUpdate = true
+            clonedUserAccounts[i].balance = sub.data?.accountUpdates.balance
+          }
+        }
+        if (!isUpdate) {
+          clonedUserAccounts.push({ name: sub.data.accountUpdates.name, balance: sub.data.accountUpdates.balance })
+        }
+        setUserAccounts(clonedUserAccounts)
+      }
+    }
+  }, [sub.data])
 
   if (loading) {
     return <div>loading...</div>
   }
-  if (!data || data.exchangeRequests?.length === 0) {
+  if (!data || data.exchangeRequests?.length === 0 || !requestsUpdates) {
     return <div>No Transfer History</div>
   }
 
   return (
     <div className="mw6">
-      {data.exchangeRequests
+      {requestsUpdates
         ?.slice(0)
         .reverse()
         .map(r => (
@@ -56,18 +95,4 @@ export function MyTransfers() {
     </div>
   )
 
-  /* return (
-    <div>
-      <br></br>
-      <br></br>
-      {requests.reverse().map(r => (
-        <div key={r.requestId}>
-          Amount Paid: {r.amountPay} {r.fromCurrency}, Amount Wanted: {r.amountWant} {r.toCurrency}, Bid Rate:
-          {r.bidRate}
-          <br></br>
-          <br></br>
-        </div>
-      ))}
-    </div>
-  ) */
 }
