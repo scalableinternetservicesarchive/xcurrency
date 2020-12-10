07fc7fe73b18cfe5442e46b7eb535f4d8ac59290
dhruvsanchety <43064513+dhruvsanchety@users.noreply.github.com>
2020-12-09 19:27:45 -0800

    Update Transfers.tsx

--- web/src/view/page/Transfers.tsx
+++ web/src/view/page/Transfers.tsx
@@ -1,11 +1,18 @@
-import { useQuery } from '@apollo/client'
+import { useQuery, useSubscription } from '@apollo/client'
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
-import { FetchExchangeRequests, FetchExchangeRequestsVariables } from '../../graphql/query.gen'
+import { useEffect } from 'react'
+import {
+  FetchExchangeRequests,
+  FetchExchangeRequestsVariables,
+  RequestSubscription,
+  RequestSubscriptionVariables
+} from '../../graphql/query.gen'
 import { UserContext } from '../auth/user'
-import { fetchExchangeRequests } from '../exchangeRequestQL/fetchExchangeRequest'
+import { fetchExchangeRequests, subscribeRequests } from '../exchangeRequestQL/fetchExchangeRequest'
 import { AppRouteParams } from '../nav/route'
 import { Page } from './Page'
+
 interface TransfersProps extends RouteComponentProps, AppRouteParams {}
 
 export function Transfers(props: TransfersProps) {
@@ -26,23 +33,61 @@ export function MyTransfers() {
       console.error(err)
     })
 */
-  const user = React.useContext(UserContext)
-  const id = user.displayId()
+  const user1 = React.useContext(UserContext)
+  const user = React.useContext(UserContext).user
+  const id = user1.displayId()
   const { loading, data } = useQuery<FetchExchangeRequests, FetchExchangeRequestsVariables>(fetchExchangeRequests, {
     variables: { id },
-    pollInterval: 1000,
+    // pollInterval: 1000,
   })
+
+  const [requests, setRequests] = React.useState(data?.exchangeRequests)
+
+  useEffect(() => {
+    setRequests(data?.exchangeRequests)
+  }, [data])
+
+  const sub = useSubscription<RequestSubscription, RequestSubscriptionVariables>(subscribeRequests, {
+    variables: { userId: user!.id },
+  })
+
+  // update according to subscription
+  useEffect(() => {
+    console.log(sub.data)
+    if (sub.data?.requestUpdates) {
+      if (requests) {
+        const clonedRequests: any[] = []
+        requests.forEach((request: any) =>
+          clonedRequests.push({
+            amountPay: request.amountPay,
+            fromCurrency: request.fromCurrency,
+            amountWant: request.amountWant,
+            toCurrench: request.toCurrency,
+            bidRate: request.bidRate,
+          })
+        )
+        clonedRequests.push({
+          amountPay: sub.data.requestUpdates.amountPay,
+          fromCurrency: sub.data.requestUpdates.fromCurrency,
+          amountWant: sub.data.requestUpdates.amountWant,
+          toCurrench: sub.data.requestUpdates.toCurrency,
+          bidRate: sub.data.requestUpdates.bidRate,
+        })
+        setRequests(clonedRequests)
+      }
+    }
+  }, [sub.data])
 
   if (loading) {
     return <div>loading...</div>
   }
-  if (!data || data.exchangeRequests?.length === 0) {
+  if (!data || data.exchangeRequests?.length === 0 || !requests) {
     return <div>No Transfer History</div>
   }
 
   return (
     <div className="mw6">
-      {data.exchangeRequests
+      {requests
         ?.slice(0)
         .reverse()
         .map(r => (
@@ -55,19 +100,5 @@ export function MyTransfers() {
         ))}
     </div>
   )
-
-  /* return (
-    <div>
-      <br></br>
-      <br></br>
-      {requests.reverse().map(r => (
-        <div key={r.requestId}>
-          Amount Paid: {r.amountPay} {r.fromCurrency}, Amount Wanted: {r.amountWant} {r.toCurrency}, Bid Rate:
-          {r.bidRate}
-          <br></br>
-          <br></br>
-        </div>
-      ))}
-    </div>
-  ) */
 }
+
